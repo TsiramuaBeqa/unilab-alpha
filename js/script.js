73793c9e6aee46f769800cef8144db26c84de9d4
TsiramuaBeqa <110886129+TsiramuaBeqa@users.noreply.github.com>
2022-09-06 08:04:31 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const beqasObject = {
+  firstName: "Beqa",
+  lastName: "Tsiramua",
+  age: 21,
+  university: "Ilia State University",
+  married: false,
+  driversLicense: true,
+  programmingLanguages: ["HTML", "CSS", "JavaScript", "Python"],
+  framework: ["Bootstrap", "Angular", "jQuery", "Flask", "Django"],
+  jsProjects: ["Tic-Tac-Toe", "Rock, Paper, Scissors", "Guess-Game", "calculator", "Calendar", "Clock", "Loto Game", "More..."],
+  htmlProject: ["Unilab 12 project", "Ecoland", "Travelo", "Facebook", "Instagram", "Twitter", "Steam", "Netflix", "Messenger", "Origin", "Vikipedia", "Unilab Website", "It Step Georgia website", "Many more..."]
+}
+console.log(beqasObject.firstName, beqasObject.lastName, beqasObject.age, beqasObject.university, beqasObject.driversLicense, beqasObject.programmingLanguages, beqasObject.framework, beqasObject.jsProjects, beqasObject.htmlProject)
